2f5025c6b19001a830838f638c9f57c4b4bd8ded
Vasilii <115224982+deletevochrome@users.noreply.github.com>
2022-10-21 13:43:53 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -53,8 +53,8 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         return (self.CALORIES_MEAN_SPEED_MULTIPLIER * 
-                self.get_mean_speed(self.LEN_STEP) + 
-                self.CALORIES_MEAN_SPEED_SHIFT) * 
+                self.get_mean_speed(self.LEN_STEP)) + 
+                (self.CALORIES_MEAN_SPEED_SHIFT) * 
                 self.weight / 1000 * (self.duration * 60)
 
 
