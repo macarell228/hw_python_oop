acb3b07f5b03c13636cf034211dbdd271d96dadc
Vasilii <115224982+deletevochrome@users.noreply.github.com>
2022-11-09 10:37:33 -0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -81,7 +81,7 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         a = self.get_mean_speed() * self.KMH_IN_MSEC
-        return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight 
+        return ((self.CALORIES_WEIGHT_MULTIPLIER * self.weight
                 + (a**2 / (self.height / self.CM_IN_M))
                 * self.CALORIES_SPEED_HEIGHT_MULTIPLIER * self.weight)
                 * self.duration * self.MIN_IN_H)
@@ -107,9 +107,9 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
-        return ((self.get_mean_speed(self.LEN_STEP) 
+        return ((self.get_mean_speed(self.LEN_STEP)
                 + self.CALORIES_MEAN_SPEED_SHIFT)
-                * self.CALORIES_WEIGHT_MULTIPLIER 
+                * self.CALORIES_WEIGHT_MULTIPLIER
                 * self.weight * self.duration)
 
 
